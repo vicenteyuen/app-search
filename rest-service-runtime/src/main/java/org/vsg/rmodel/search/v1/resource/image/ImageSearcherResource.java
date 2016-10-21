4f8c5df93fb56ce9affd648fc4388e54cdee5815
Vicente Yuen <ruanweibiao@modernavenue.com>
2016-10-21 16:24:56 +0800

    change log

--- rest-service-runtime/src/main/java/org/vsg/rmodel/search/v1/resource/image/ImageSearcherResource.java
+++ rest-service-runtime/src/main/java/org/vsg/rmodel/search/v1/resource/image/ImageSearcherResource.java
@@ -1,31 +1,49 @@
 package org.vsg.rmodel.search.v1.resource.image;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
+import javax.ws.rs.container.TimeoutHandler;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.vsg.common.fileupload.FileItem;
+import org.vsg.common.fileupload.FileUpload;
+import org.vsg.common.fileupload.FileUploadException;
+import org.vsg.common.fileupload.RequestContext;
+import org.vsg.common.fileupload.disk.DiskFileItemFactory;
+import org.vsg.common.fileupload.jaxrs.JaxrsRequestContextBuilder;
 import org.vsg.common.i18n.I18nMesssageSupport;
 
 @Path("/search/image")
 @Consumes({
-	MediaType.APPLICATION_JSON
+	MediaType.MULTIPART_FORM_DATA
 })
 public class ImageSearcherResource {
 	
 	@Inject
 	private I18nMesssageSupport i18n;
-	
+
+	@Inject	
+	@Named("client.image.uploadtmp")	
+	private String clientImageUploadPath;	
 	
 	private Logger logger = LoggerFactory.getLogger( ImageSearcherResource.class );
 	
@@ -35,20 +53,61 @@ public class ImageSearcherResource {
 	 * @param asyncResponse
 	 * @throws InterruptedException
 	 */
-	@GET
-	public void imageSearch(@Suspended AsyncResponse asyncResponse) throws InterruptedException {
+	@POST
+	public void imageSearch(
+			@Context HttpHeaders headers,
+			@QueryParam("reqId") String reqId,
+			InputStream fileIs ,  @Suspended AsyncResponse asyncResponse ) throws InterruptedException {
 		
 		long startTime = System.currentTimeMillis();
 
+		// --- handle time out ---
+		asyncResponse.setTimeoutHandler( new TimeoutHandler() {
+
+			@Override
+			public void handleTimeout(AsyncResponse asyResponse) {
+				logger.info("reached timeout");
+				asyResponse.resume( Response.ok().build() );
+			}
+			
+		});
+
 		// --- set time out ---
-		asyncResponse.setTimeout(2, TimeUnit.SECONDS);
+		asyncResponse.setTimeout(5, TimeUnit.SECONDS);
 		
 		
-		String msg = i18n.getMessage("label.title", null);
+		// ---- create tmp file ----
+		try {
+
+			
+			DiskFileItemFactory factory = new DiskFileItemFactory();
+			factory.setRepository( new File(clientImageUploadPath) );
+			
+			FileUpload fileUpload = new FileUpload( factory );
+			
+			RequestContext reqContext = JaxrsRequestContextBuilder.createRequestContext(headers, fileIs);
+			List<FileItem>  fileItems =  fileUpload.parseRequest( reqContext );
+			
+			Map<String, List<FileItem>>  mapping =  fileUpload.parseParameterMap(reqContext);
+			
+			if (fileItems.size() == 0) {
+				// ---- break the item handle ---
+			}
+			FileItem  fileItem = fileItems.get(0);
+			// --- search files record ---
+		
+
+
+		} catch (IOException | FileUploadException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+
 		
 		
 		Map result = new HashMap();
-		result.put("ok", msg);
+		//result.put("ok", msg);
 		
 		Response jaxrs = Response.ok( result ).type( MediaType.APPLICATION_JSON ).build();
 
--- war-search/src/main/java/org/vsg/rmodel/module/ConfigrationModule.java
+++ war-search/src/main/java/org/vsg/rmodel/module/ConfigrationModule.java
@@ -20,19 +20,6 @@ public class ConfigrationModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
-		
-		Configuration configration = loadConfigration();
-		
-		// --- set configration handle ---
-		bind(String.class).annotatedWith(Names.named("JDBC.driver")).toInstance(configration.getString("oracle.driverClassName"));
-		bind(String.class).annotatedWith(Names.named("JDBC.url")).toInstance(configration.getString("oracle.url"));				
-		bind(String.class).annotatedWith(Names.named("JDBC.username")).toInstance(configration.getString("oracle.username"));						
-		bind(String.class).annotatedWith(Names.named("JDBC.password")).toInstance(configration.getString("oracle.password"));
-		
-		
-		bind(String.class).annotatedWith(Names.named("index.image.path")).toInstance(configration.getString("index.image.path"));
-		bind(String.class).annotatedWith(Names.named("image.orgin.path")).toInstance(configration.getString("image.orgin.path"));		
-		
 
 	}
 	
@@ -49,31 +36,5 @@ public class ConfigrationModule extends AbstractModule {
 		
 		return provider;
 	}
-	
-	
-	private static Configuration loadConfigration() {
-		Parameters params = new Parameters();
-		// Read data from this file
-		File propertiesFile = new File("config.properties");
-
-		FileBasedConfigurationBuilder<FileBasedConfiguration> builder =
-		    new FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
-		    .configure(params.fileBased()
-		        .setFile(propertiesFile));
-		try
-		{
-		    Configuration config = builder.getConfiguration();
-		    return config;
-
-		    // config contains all properties read from the file
-		}
-		catch(ConfigurationException cex)
-		{
-		    // loading of the configuration file failed
-			cex.printStackTrace();
-		}
-		return null;
-				
-	}		
 
 }
